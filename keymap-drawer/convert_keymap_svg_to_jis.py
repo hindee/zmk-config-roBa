a23c7c5926bf5d10b20993c47396c7b656234adf
okaka <75552586+hindee@users.noreply.github.com>
2025-06-21 17:11:56 +0900

    Update convert_keymap_svg_to_jis.py

--- keymap-drawer/convert_keymap_svg_to_jis.py
+++ keymap-drawer/convert_keymap_svg_to_jis.py
@@ -28,7 +28,7 @@ us_to_jis = {
 }
 
 # SVGファイルを読み込む
-with open('roBa.svg', 'r', encoding='utf-8') as file:
+with open('keymap-drawer/roBa.svg', 'r', encoding='utf-8') as file:
     svg_data = file.read()
 
 # 定義に基づいて置き換えを行う
@@ -38,7 +38,7 @@ for us_key, jis_value in sorted(us_to_jis.items(), key=lambda x: -len(x[0])):
     svg_data = re.sub(pattern, replacement, svg_data)
 
 # 置き換え後のSVGデータを保存する
-with open('roBa_jis.svg', 'w', encoding='utf-8') as file:
+with open('keymap-drawer/roBa_jis.svg', 'w', encoding='utf-8') as file:
     file.write(svg_data)
 
 print("置き換えが完了しました。")
