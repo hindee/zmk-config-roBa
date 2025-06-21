bb79a8094a4207b1e7cf71774126de7fbbf71340
okaka <75552586+hindee@users.noreply.github.com>
2025-06-22 03:36:02 +0900

    Update convert_keymap_svg_to_jis.py

--- keymap-drawer/convert_keymap_svg_to_jis.py
+++ keymap-drawer/convert_keymap_svg_to_jis.py
@@ -4,17 +4,17 @@ import html
 # 定義を辞書に格納
 us_to_jis = {
     '"': '*',       # DOUBLE_QUOTES → *
-    '&': "'",       # AMPERSAND → '
-    "'": ':',       # SINGLE_QUOTE → :
+    '&amp;': "&apos;",       # AMPERSAND → '
+    "&aposj;": ':',       # SINGLE_QUOTE → :
     '=': '^',       # EQUAL → ^
-    '^': '&',       # CARET → &
-    '¥': '_',       # LS(0x87) or 0x89 → _
+    '^': '&amp;',       # CARET → &
+    'Sft+0x87': '_',       # LS(0x87) or 0x89 → _
+    '0x89': '_',  # 0x89 → _
     '+': '~',       # PLUS → ~
     '~': '半角/全角', # GRAVE → 半角/全角
     '|': '}',       # PIPE → }
     '@': '"',       # AT → "
     ':': '+',       # COLON → +
-    '*': '(',       # ASTERISK → (
     '`': ']',       # BACKQUOTE → ]
     '_': '=',       # UNDER → =
     '[': '@',       # LEFT_BRACKET → @
@@ -22,24 +22,30 @@ us_to_jis = {
     ']': '[',       # RIGHT_BRACKET → [
     '\\': ']',      # BACKSLASH → ]
     '(': ')',       # LEFT_PARENTHESIS → )
-    ')': '(',       # ASTERISK → (
+    '*': '(',       # ASTERISK → (
     '}': '{',       # RIGHT_BRACE → {
+    'Sft+0x89': '|',  # Sht+\ → |
     'LANGUAGE_1': 'かな',  # かな
     'LANGUAGE_2': '英数',  # 英数
-    '~': '半角/全角'  # 半角/全角
+    '~': '^'  # 半角/全角
 }
 
 # SVGファイルを読み込む
 with open('keymap-drawer/roBa.svg', 'r', encoding='utf-8') as file:
     svg_data = file.read()
 
-# 定義に基づいて置き換えを行う
-for us_key, jis_value in sorted(us_to_jis.items(), key=lambda x: -len(x[0])):
-    # jis_value を SVG/XMLエスケープ
-    escaped_jis_value = html.escape(jis_value, quote=True)
-    pattern = f'>{re.escape(us_key)}<'
-    replacement = f'>{escaped_jis_value}<'
-    svg_data = re.sub(pattern, replacement, svg_data)
+# 置換対象パターンを「>キー<」の形で正規表現化
+# キーの長い順でソートして優先度を担保
+keys_sorted = sorted(us_to_jis.keys(), key=lambda x: -len(x))
+pattern = re.compile(r'>(' + '|'.join(re.escape(k) for k in keys_sorted) + r')<')
+
+def replacer(match):
+    key = match.group(1)
+    # SVG/XMLエスケープ
+    replacement = html.escape(us_to_jis[key], quote=True)
+    return f'>{replacement}<'
+
+svg_data = pattern.sub(replacer, svg_data)
 
 # 置き換え後のSVGデータを保存する
 with open('keymap-drawer/roBa.svg', 'w', encoding='utf-8') as file:
